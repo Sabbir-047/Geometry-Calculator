a09ed95ebd3b38221b492d11a86915ab77570727
Sabbir-047 <sabbirbhuiyan026@gmail.com>
2023-10-12 07:42:05 +0600

    Dynamic HTML added

--- js/script.js
+++ js/script.js
@@ -22,6 +22,8 @@ function triangleCalculator(){
     // show area
     const triangleArea = document.getElementById('triangle-area');
     triangleArea.innerText = areaTri;
+
+    addToCalculationEntry('Triangle', areaTri);
 }
 
 
@@ -43,6 +45,8 @@ function rectangleCalculator(){
     // show area
     const rectangleArea = document.getElementById('rectangle-area');
     rectangleArea.innerText = areaRect;
+
+    addToCalculationEntry('Rectangle', areaRect);
 }
 
 
@@ -60,7 +64,15 @@ function parallelogramCalculator(){
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height');
     const area = base * height;
+    // validate
+    if(isNaN(base) || isNaN(height)){
+        alert('Please input a valid number');
+        return;
+    }
     setElementInnerText('parallelogram-area', area);
+
+    // add to calculation entry (optional for now)
+    addToCalculationEntry('parallelogram', area);
 }
 
 // Ellipse Calculator
@@ -70,6 +82,7 @@ function ellipseCalculator(){
     const area = 3.1416 * base * height;
     const areaTwoDecimal = area.toFixed(2);
     setElementInnerText('ellipse-area', areaTwoDecimal);
+    addToCalculationEntry('Ellipse', areaTwoDecimal);
 }
 
 // reusable input value field in number
@@ -94,3 +107,30 @@ function setElementInnerText(elementId, area){
  * 2. check type using typeof
  * 3. NaN means: not a number. isNaN is checking whether the input is not a number or not
  */
+
+
+
+// data entry (optional for now)
+// add to calculation entry
+
+/**
+ * 1. get the element where you want to add the dynamic HTML
+ * 2. create an element you want to add
+ * 3. if needed add some class
+ * 4. set inner HTML. it could be dynamic template string
+ * 5. append the created element as a child of the parent
+ * 
+ */
+
+function addToCalculationEntry(areaType, area){
+    const calculationEntry = document.getElementById('calculation-entry');
+    const p = document.createElement('p');
+
+    const count = calculationEntry.childElementCount;
+
+    // p.innerHTML = areaType + ' ' + area; 
+    // alternative ekhane aro dynamic kora jabe
+    p.classList.add('my-4') /** eta nicher dynamic jinish er moddhe gap */
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2<sup> <button class="btn btn-info btn-sm">Convert to m</button>`;
+    calculationEntry.appendChild(p);
+}
